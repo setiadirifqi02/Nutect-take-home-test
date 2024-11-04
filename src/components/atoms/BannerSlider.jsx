854605bca6553a3cee8b21b77a719f4e3883e822
Rifqi Setiadi <setiadirifqi2638@gmail.com>
2024-11-04 16:42:32 +0700

    hide scroll bar's web  browser

--- src/components/atoms/BannerSlider.jsx
+++ src/components/atoms/BannerSlider.jsx
@@ -1,6 +1,6 @@
 const BannerSlider = ({ banner }) => {
   return (
-    <div className="Banner__slider flex gap-4 overflow-x-scroll">
+    <div className="Banner__slider flex gap-4 overflow-x-scroll no-scrollbar">
       {banner?.map((item) => (
         <img src={item?.banner_image} alt={item?.banner_name} />
       ))}
