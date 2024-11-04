a646d1f541a11bcf40dbc4d9462f997b9deeabc5
Rifqi Setiadi <setiadirifqi2638@gmail.com>
2024-11-04 17:05:00 +0700

    add key to banner Slider component

--- src/components/atoms/BannerSlider.jsx
+++ src/components/atoms/BannerSlider.jsx
@@ -2,7 +2,11 @@ const BannerSlider = ({ banner }) => {
   return (
     <div className="Banner__slider flex gap-4 overflow-x-scroll no-scrollbar">
       {banner?.map((item) => (
-        <img src={item?.banner_image} alt={item?.banner_name} />
+        <img
+          key={item?.banner_name}
+          src={item?.banner_image}
+          alt={item?.banner_name}
+        />
       ))}
     </div>
   );
